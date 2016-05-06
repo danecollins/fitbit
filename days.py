6e15f99e0293cde9aafc7a6ee39e2a9db146f4a1
Dane Collins <dane@pobox.com>
2016-05-06 15:10:40 -0700

    Updated to new db format and converted option parsing to OptionParse

--- days.py
+++ days.py
@@ -1,24 +1,38 @@
-#!python
+"""
+For a given users this prints out the range of dates for which
+the database has data.
+
+It is useful to run before doing a download to know where to
+start.
+"""
+
 from __future__ import print_function
 from __future__ import unicode_literals
 from fbcache import FitbitCache
-from sys import argv
+from optparse import OptionParser
 
-##################################################################
-#
-# For a given users this prints out the range of dates for which
-# the database has data.
-#
-# It is useful to run before doing a download to know where to
-# start.
 
-if len(argv) != 2:
-    print('Usage: python days.py user')
+usage = "usage: %prog user-name"
+parser = OptionParser(usage)
+
+(options, args) = parser.parse_args()
+
+if len(args) != 1:
+    parser.print_help()
+    exit(1)
+else:
+    user_name = args[0]
+
+cache = FitbitCache(user_name)
+
+if not cache.data_exists():
+    print("ERROR: Could not open data for user '{}'\n".format(user_name))
+    parser.print_help()
     exit(1)
 
+cache.read()
+days = cache.daylist()
 
-fbd = FitbitCache()
-fbd.read()
-days = fbd.daylist()
-print('Start = {}'.format(days[0]))
-print('End   = {}'.format(days[-1]))
+print('Start    = {}'.format(days[0]))
+print('End      = {}'.format(days[-1]))
+print('Num Days = {}'.format(cache.num_days()))
