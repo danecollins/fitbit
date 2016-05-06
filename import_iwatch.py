cfa2d24b5b2d793ac2f55021ded8b528e3e353ce
Dane Collins <dane@pobox.com>
2016-05-06 15:13:19 -0700

    Convert to using OptionParser and change name of imported property.

--- import_iwatch.py
+++ import_iwatch.py
@@ -18,21 +18,39 @@ from __future__ import unicode_literals
 
 # standard python includes
 import csv
+import datetime
 
 # my includes
 from fbcache import FitbitCache
 
-cache = FitbitCache()
+from optparse import OptionParser
+
+
+usage = "usage: %prog user-name"
+parser = OptionParser(usage)
+
+(options, args) = parser.parse_args()
+
+if len(args) != 1:
+    parser.print_help()
+    exit(1)
+else:
+    user_name = args[0]
+
+cache = FitbitCache(user_name)
 cache.read()
 
 
-with open('Health Data.csv', 'U') as csvfile:
+with open('Health_Data.csv', 'U') as csvfile:
     csvrecords = csv.DictReader(csvfile, dialect='excel')
+    next(csvrecords)  # skip header
     for record in csvrecords:
-        day = record['Start'][0:-5]
+        day = record['Start'][0:-6]
+        day_as_dt = datetime.datetime.strptime(day, '%d-%b-%Y')
+
         dist = record['Distance (mi)']
         steps = record['Steps (count)']
-        cache.add_item(day, 'watch_steps', steps)
-        cache.add_item(day, 'watch_dist', dist)
+        cache.add_item(day_as_dt, 'steps_aw', float(steps))
+        cache.add_item(day_as_dt, 'dist_aw', float(dist))
 
 cache.write()
