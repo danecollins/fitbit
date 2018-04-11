ece21a81b70faef4769b6e72eee0a3ad624ee8cb
Dane Collins <dane@pobox.com>
2018-04-11 07:57:12 -0700

    Allow multiple users in long format

--- db2long.py
+++ db2long.py
@@ -1,29 +1,42 @@
-from __future__ import print_function
-from __future__ import unicode_literals
+""" This exports the database as a csv file in the following format
+
+    | date    |  user  | activity   | attribute   | value  |
+
+    where activity will be 'walking' for fitbit data and is extensible
+    to other activities.
+
+    Multiple users can be output to the same file.
+"""
 
 import sys
 import fbcache
-import os
 
 
 def fb_data_to_long():
     if len(sys.argv) < 2:
-        print('\nUsage: python db2long.py base_filename user1 [user2 ...]')
+        print('\nUsage: python db2long.py filename user1 [user2 ...]')
         exit(0)
 
-    (name, ext) = os.path.splitext(sys.argv[1])
+    # (name, ext) = os.path.splitext(sys.argv[1])
+    filename = sys.argv[1]
     users = sys.argv[2:]
 
-    for user in users:
-        fn = '{}_{}{}'.format(name, user, ext)
-        with open(fn, 'w') as fp:
+    with open(filename, 'w') as fp:
+        # print header
+        print('date,user, activity,attribute,value', file=fp)
+
+        for user in users:
             db = fbcache.FitbitCache(user)
             db.read()
             for date_str, data in db.items():
                 for k, v in data.items():
-                    if k not in ['actcal', 'active1', 'active2', 'active3', 'margcal', 'sedentary', 'date', 'who']:
-                        if not (k == 'weight' and v == 0):  # don't output weights of 0
-                            print('{},{},{}'.format(date_str, k, v), file=fp)
+                    if k not in ['actcal', 'active1', 'active2', 'active3', 'margcal',
+                                 'sedentary', 'date', 'who']:
+                        if (k == 'weight') and (v == 0):
+                                continue  # don't output weights of 0
+                        else:
+                            print('{},{},{},{},{}'.format(date_str, user, 'walking', k, v),
+                                  file=fp)
 
 
 if __name__ == '__main__':
