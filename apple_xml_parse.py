563898fd7f882c67407b090578428f54e9bf4833
Dane Collins <dane@dacxl.com>
2019-02-13 19:43:36 -0800

    add hr file write

--- apple_xml_parse.py
+++ apple_xml_parse.py
@@ -1,16 +1,26 @@
 import re
 import json
 import pdb
+from collections import Counter, defaultdict
 
 
 def map_source(s):
     return {
         "Dane’s iPhone": 'Phone',
-        'Dane Watch 2': 'WatchS2',
-        "Dane's Apple\xa0Watch": 'WatchS1',
-        'Dane Watch 4': 'WatchS4',
-        'Dane_iP8': 'PhoneM8' 
+        'Dane Watch 2': 'Watch',
+        "Dane's Apple\xa0Watch": 'Watch',
+        'Dane Watch 4': 'Watch',
+        'Dane_iP8': 'Phone',
+        'Health': 'HealthApp',
+        'BP': 'BP',
+        'OMRON connect': 'BP'
     }.get(s)
+
+
+def get_date(s):
+    # pull date from string
+    return s[:10]
+
 
 hr_pat = re.compile('<Record type="HKQuantityTypeIdentifierHeartRate".*sourceName="([^"]*)".*startDate="([^"]*)" .*value="([\.\de+-]+)".*>')
 rhr_pat = re.compile('<Record type="HKQuantityTypeIdentifierRestingHeartRate".*sourceName="([^"]*)".*startDate="([^"]*)".*endDate="([^"]*)" .*value="([\.\de+-]+)".*>')
@@ -27,14 +37,15 @@ cy2_pat = re.compile('<Workout workoutActivityType="HKWorkoutActivityTypeCycling
 def_pat = re.compile('<Record type="([^"]*)".*')
 printed = set(['HKQuantityTypeIdentifierHeight', 'HKQuantityTypeIdentifierBodyMass'])
 
-c = 0
-output = []
 
-fout = open('apple_data.txt', 'w')
-fm = open('skipped_lines.txt', 'w')
-print('[', file=fout)
-try:
-    with open('/tmp/export.xml') as fp:
+def read_xml(fn):
+    c = 0
+    output = []
+    o = defaultdict(Counter)
+    fout = open('apple_data.txt', 'w')
+    fm = open('daily_data.csv', 'w')
+    fhr = open('apple_hr_data.csv', 'w')
+    with open(fn) as fp:
         for line in fp.readlines():
             c += 1
 
@@ -44,27 +55,49 @@ try:
             if hr_pat.match(line):
                 m = hr_pat.match(line)
                 d = dict(type='hr', source=map_source(m.group(1)), start=m.group(2), value=m.group(3))
+                print('{},{},{}'.format(d['source'], d['start'], d['value']), file=fhr)
+                d = None
             elif rhr_pat.match(line):
                 m = rhr_pat.match(line)
                 d = dict(type='restinghr', source=map_source(m.group(1)), start=m.group(2), end=m.group(3), value=m.group(4))
             elif st_pat.match(line):
                 m = st_pat.match(line)
                 d = dict(type='step', source=map_source(m.group(1)), start=m.group(2), end=m.group(3), value=m.group(4))
+                if d['source'] == 'Watch':
+                    _dt = get_date(d['start'])
+                    o['steps_per_day'][_dt] += float(d['value'])
+                d = None
             elif fc_pat.match(line):
                 m = fc_pat.match(line)
                 d = dict(type='flights', source=map_source(m.group(1)), start=m.group(2), end=m.group(3), value=m.group(4))
+                if d['source'] == 'Watch':
+                    _dt = get_date(d['start'])
+                    o['flights'][_dt] += float(d['value'])
+                d = None
             elif dst_pat.match(line):
                 m = dst_pat.match(line)
                 d = dict(type='distance', source=map_source(m.group(1)), start=m.group(2), end=m.group(3), value=m.group(4))
+                if d['source'] == 'Watch':
+                    _dt = get_date(d['start'])
+                    o['distance'][_dt] += float(d['value'])
+                d = None
             elif cyc_pat.match(line):
                 m = cyc_pat.match(line)
                 d = dict(type='cycling', source=map_source(m.group(1)), start=m.group(2), end=m.group(3), value=m.group(4))
+                if d['source'] == 'Watch':
+                    _dt = get_date(d['start'])
+                    o['cycling'][_dt] += float(d['value'])
+                d = None
             elif cy2_pat.match(line):
                 m = cy2_pat.match(line)
-                d = dict(type='cycling_workout', duration=m.group(1), source=m.group(3), start=m.group(4), end=m.group(5), value=m.group(2))
+                d = dict(type='cycling_workout', duration=m.group(1), source=m.group(3), start=m.group(4), value=m.group(2))
             elif cal_pat.match(line):
                 m = cal_pat.match(line)
                 d = dict(type='calories', source=map_source(m.group(1)), start=m.group(2), end=m.group(3), value=m.group(4))
+                if d['source'] == 'Watch':
+                    _dt = get_date(d['start'])
+                    o['calories'][_dt] += float(d['value'])
+                d = None
             elif sys_pat.match(line):
                 m = sys_pat.match(line)
                 d = dict(type='systolic', source=map_source(m.group(1)), start=m.group(2), value=m.group(3))
@@ -95,13 +128,21 @@ try:
             elif '<ActivitySummary' in line:
                 pass
             else:
-                print(line, file=fm)
+                pass
             if d:
                 output.append(d)
 
-
+    print('writing apple_data.txt json file')
     fout.write(json.dumps(output, indent=4))
     fout.close()
+    print('writing daily_data.csv')
+    for typ, cntr in o.items():
+        for dy, v in cntr.items():
+            print("{},{},{}".format(dy, typ, v), file=fm)
     fm.close()
-except:
-    pdb.set_trace()
+    fhr.close()
+    print('writing apple_hr_data.csv')
+    return o
+
+
+read_xml('/tmp/export.xml')
---
+++ awcsv_to_db.py
@@ -0,0 +1,21 @@
+import fbcache
+
+
+cache = fbcache.FitbitCache('dane')
+cache.read()
+
+m = {
+    'steps_per_day': 'steps_aw',
+    'cycling': 'cycling_aw',
+    'distance': 'distance_aw',
+    'flights': 'floors_aw',
+    'calories': 'calories_aw'
+}
+
+with open('daily_data.csv') as fp:
+    for line in fp.readlines():
+        line = line.strip()
+        day, k, v = line.split(',')
+        cache.add_item(day, m[k], round(float(v), 2))
+
+cache.write()
