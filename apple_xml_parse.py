1f7b5a235d6251af8d14ef8a73d062fd56d9ba02
Dane Collins <dane@dacxl.com>
2021-05-11 10:25:08 -0700

    add resting hr to daily data, rename file

--- apple_xml_parse.py
+++ apple_xml_parse.py
@@ -25,11 +25,15 @@ def map_source(s):
         'Dane Watch 4': 'Watch',
         'Dane Watch 5': 'Watch',
         "Dane’s Apple Watch": 'Watch',
+        "Dane's Apple\u00a0Watch": 'Watch',
+        "Dane\u2019s Apple\u00a0Watch": 'Watch',
         'Dane_iP8': 'Phone',
         'Dxi': 'Phone',
         'Health': 'HealthApp',
         'BP': 'BP',
-        'OMRON connect': 'BP'
+        'OMRON connect': 'BP',
+        'Strava': 'Strava',
+        'Connect': 'Garmin',
     }.get(s)
 
 
@@ -53,13 +57,14 @@ cy2_pat = re.compile('<Workout workoutActivityType="HKWorkoutActivityTypeCycling
 def_pat = re.compile('<Record type="([^"]*)".*')
 printed = set(['HKQuantityTypeIdentifierHeight', 'HKQuantityTypeIdentifierBodyMass'])
 
-
+import pdb
 def read_xml(fn):
+    print('The following data identifiers were not processed:')
     c = 0
     output = []
     o = defaultdict(Counter)
     fout = open('apple_data.txt', 'w')
-    fm = open('daily_data.csv', 'w')
+    fm = open('apple_daily_data.csv', 'w')
     fhr = open('apple_hr_data.csv', 'w')
     with open(fn) as fp:
         for line in fp.readlines():
@@ -70,17 +75,24 @@ def read_xml(fn):
 
             if hr_pat.match(line):
                 m = hr_pat.match(line)
+                # hr data can come from watch (both hr app and ekg app) as well as BP monitor
+                # ekg app gives floating point hr
                 source = map_source(m.group(1))
-                assert source in ['Watch', 'Phone', 'HealthApp', 'BP'], 'Illegal source: {}'.format(line)
+                assert source in ['Watch', 'Phone', 'HealthApp', 'BP'], 'Illegal source for heart rate: {}'.format(line)
                 d = dict(type='hr', source=source, start=m.group(2), value=m.group(3))
-                print('{},{},{}'.format(d['source'], d['start'], d['value']), file=fhr)
+                print('{},{},{},{}'.format(d['type'], d['source'], d['start'], d['value']), file=fhr)
                 d = None
             elif rhr_pat.match(line):
                 m = rhr_pat.match(line)
                 d = dict(type='restinghr', source=map_source(m.group(1)), start=m.group(2), end=m.group(3), value=m.group(4))
+                # add to daily dictionary
+                _dt = get_date(d['start'])
+                o['restinghr'][_dt] = float(d['value'])
+                d = None
             elif st_pat.match(line):
                 m = st_pat.match(line)
                 d = dict(type='step', source=map_source(m.group(1)), start=m.group(2), end=m.group(3), value=m.group(4))
+                # sum all the step entries to get steps per day
                 if d['source'] == 'Watch':
                     _dt = get_date(d['start'])
                     o['steps_per_day'][_dt] += float(d['value'])
@@ -108,7 +120,12 @@ def read_xml(fn):
                 d = None
             elif cy2_pat.match(line):
                 m = cy2_pat.match(line)
-                d = dict(type='cycling_workout', duration=m.group(1), source=m.group(3), start=m.group(4), value=m.group(2))
+                source = map_source(m.group(3))
+                if source not in ['Watch', 'Phone', 'Strava', 'Garmin']:
+                    pdb.set_trace()
+
+                assert source in ['Watch', 'Phone', 'Strava', 'Garmin'], 'Illegal source for cycling workout: [{}]\n{}'.format(source, line)
+                d = dict(type='cycling_workout', duration=m.group(1), source=source, start=m.group(4), value=m.group(2))
             elif cal_pat.match(line):
                 m = cal_pat.match(line)
                 d = dict(type='calories', source=map_source(m.group(1)), start=m.group(2), end=m.group(3), value=m.group(4))
@@ -125,10 +142,12 @@ def read_xml(fn):
             elif def_pat.match(line):
                 m = def_pat.match(line).group(1)
                 if 'HKQuantityTypeIdentifierDistanceWalkingRunning' in line:
-                    print(line)
+                    print('*', line)
                 if m not in printed:
-                    print(m)
+                    print('*', m)
                     printed.add(m)
+
+            # these are the ignored identifiers
             elif line.startswith('<MetadataEntry'):
                 pass
             elif line.startswith('<InstantaneousBeatsPerMinute'):
